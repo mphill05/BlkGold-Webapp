c2add90aeab7ccedd43ebb2428615cfb08357d38
Mark Phillips <mphill05@gmail.com>
2021-06-12 01:03:31 -0700

    updates to home page with new video content and update to video page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,13 +6,13 @@ import { Container } from "react-bootstrap";
 const HomePage = () => (
   <Container fluid sm={12} md={12} lg={12} className="homeContainer">
     <iframe
-      title='New Music'
-      width="100%"
-      height="100%"
-      scrolling="no"
-      frameborder="no"
-      allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1046524978&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/y3jwRGLNBoM"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen>
     </iframe>
   </Container>
 );
--- src/pages/video/video.component.jsx
+++ src/pages/video/video.component.jsx
@@ -1,50 +1,8 @@
 import React from "react";
-import video1Img from "../../assets/crwth.jpg";
 
 import "./video.styles.scss";
-import { Container, Col, Row, Modal, Button } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
-class MusicPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false,
-      modalTitle: "",
-      coverArt: "",
-      infoContent: "",
-    };
-  }
-
-  defaultModal() {
-    this.setState({
-      show: false,
-      modalTitle: "",
-      coverArt: "",
-      infoContent: "",
-    });
-  }
-
-  handleModal1() {
-    this.setState({
-      show: !this.state.show,
-      modalTitle: "Ultimate",
-      modalCoverArt: video1Img,
-      infoContent: [
-        <li>
-          <a
-            href="https://open.spotify.com/album/56NJFT4FRqP04hM85vU8pF?si=n3Jgg_7-RS2Q4vKUnOf_TA"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faYoutube} size="3x" />
-          </a>
-        </li>,
-      ],
-    });
-  }
+class VideoPage extends React.Component {
 
   render() {
     return (
@@ -53,48 +11,18 @@ class MusicPage extends React.Component {
           <strong>VIDEOS</strong>
           <hr />
         </div>
-        <Container fluid className="videos">
-          <Row>
-            <Col
-              onClick={() => {
-                this.handleModal1();
-              }}
-              sm={12}
-              md={6}
-              className="coverArt"
-            >
-              <img src={video1Img} alt="Video Still" />;
-            </Col>
-          </Row>
-        </Container>
-
-        {/* Modal */}
-        <Modal className="my-modal" show={this.state.show} size="lg" centered>
-          <Modal.Header>
-            <h5 ref="header text">{this.state.modalTitle}</h5>
-            <Button
-              className="timesFA"
-              onClick={() => {
-                this.defaultModal();
-              }}
-            >
-              <FontAwesomeIcon icon={faTimes} />
-            </Button>
-          </Modal.Header>
-          <Modal.Body className="modalBody">
-            <img
-              src={this.state.modalCoverArt}
-              className="img-fluid"
-              alt="Cover Art"
-            />
-          </Modal.Body>
-          <Modal.Footer className="modalFooter">
-            <ul ref="downloadLinks">{this.state.infoContent}</ul>
-          </Modal.Footer>
-        </Modal>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/y3jwRGLNBoM"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
       </div>
     );
   }
 }
 
-export default MusicPage;
+export default VideoPage;
