28fe9afdf60da8cb70d4377d956255f526105945
Mark Phillips <mphill05@gmail.com>
2021-06-24 08:55:50 -0700

    video

--- src/pages/video/video.component.jsx
+++ src/pages/video/video.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "./video.styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 class VideoPage extends React.Component {
 
@@ -11,6 +13,19 @@ class VideoPage extends React.Component {
           <strong>VIDEOS</strong>
           <hr />
         </div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/y3jwRGLNBoM"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+        <div>
+          <h1>downloadLinks</h1>
+        </div>
+        <hr />
         <iframe
           width="560"
           height="315"
